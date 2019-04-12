b10a105c4d852ff669e741693fb9fb571cbb753a
gmidorii <stitow14@gmail.com>
2019-04-12 23:54:57 +0900

    Add /signup path and data class for form.

--- src/main/kotlin/com/github/isucon/web/CustomController.kt
+++ src/main/kotlin/com/github/isucon/web/CustomController.kt
@@ -1,8 +1,10 @@
 package com.github.isucon.web
 
+import javafx.beans.NamedArg
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 
@@ -10,7 +12,21 @@ import javax.servlet.http.HttpServletResponse
 class CustomController {
 
     @GetMapping("/ping")
-    fun ping(res: HttpServletResponse): ResponseEntity<String> {
+    fun ping(): ResponseEntity<String> {
        return ResponseEntity("ok", HttpStatus.OK)
     }
+
+    @PostMapping("/signUp")
+    fun signUp(form: SignUpForm): ResponseEntity<String> {
+        println(form)
+        return ResponseEntity("ok", HttpStatus.OK)
+    }
 }
+
+data class SignUpForm (
+        var name: String = "",
+        @NamedArg("bank_id")
+        var bankId: String = "",
+        var password: String = ""
+)
+
